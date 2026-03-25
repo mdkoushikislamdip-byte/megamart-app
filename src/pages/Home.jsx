f222b9224d46dec85d7ee9749d92b4d02402088a
Md Koushik Islam Dip <mdkoushikislamdip@gmail.com>
2026-03-26 02:30:05 +0600

    3rd commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,20 @@
 import React from "react";
 import Navbar from "../components/Layout/Navbar";
-<<<<<<< HEAD
-import Banner from "../components/home/Banner";
-import ProductCards from "../components/home/ProductCards";
-import Category from "../components/home/Category";
-import TopElectronicsBrand from "../components/home/TopElectronicsBrand";
-=======
 import Banner from "../components/shop/Banner";
 import BestDeals from "../components/home/BestDeals";
 import Category from "../components/home/Category";
 import TopElectronicsBrand from "../components/home/TopElectronicsBrand";
 import DailyEssentials from "../components/home/DailyEssentials";
->>>>>>> 2e434f8 (3rd submission of the project)
 
 
 const Home = () => {
   return (
     <div>
       <Banner />
-<<<<<<< HEAD
-      <ProductCards />
-      <Category />
-      <TopElectronicsBrand />
-=======
       <BestDeals />
       <Category />
       <TopElectronicsBrand />
       <DailyEssentials />
->>>>>>> 2e434f8 (3rd submission of the project)
     </div>
   );
 };
