0468ce780c91bf29a0a40bcd20078bec8ecf10b4
Md Koushik Islam Dip <mdkoushikislamdip@gmail.com>
2026-03-26 23:44:01 +0600

    remove font family

--- src/components/home/DailyEssentials.jsx
+++ src/components/home/DailyEssentials.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router";
 import { BiChevronRight } from "react-icons/bi";
 import { useFetchProductsQuery } from "../../services/Api";
 
+
 const DailyEssentials = () => {
   const { data, isLoading, isError, error } = useFetchProductsQuery({
     limit: 7,
